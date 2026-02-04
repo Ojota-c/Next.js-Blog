285dfe558718387e07bcd1137cb41c1011a4c688
joao <joaocb202@gmail.com>
2026-02-04 16:16:20 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import { Header } from '../components/Header';
+import clsx from 'clsx';
+import { SpinLoader } from '../components/SpinLoader';
 
 export default function HomePage() {
-  console.log('HomePage')
+  console.log('HomePage');
   return (
     <div>
-      <Header/>
+      <SpinLoader containerClasses={clsx('min-h-125', 'bg-amber-400')} />
     </div>
   );
 }
