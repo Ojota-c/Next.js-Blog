7c0f7179e5c65f86c5f8c20e2c3a0678e7d2ba70
joao <joaocb202@gmail.com>
2026-02-27 14:17:56 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,11 +11,33 @@ export default async function HomePage() {
     <Container>
       <Header />
 
-      <section>
-        <Link href='#'>
-          <Image src='' width={1200} height={720} alt='Título do post' />
+      <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
+        <Link className='w-full h-full overflow-auto rounded-xl' href='#'>
+          <Image
+            className='group-hover:scale-105 transition duration-800 '
+            src='/images/image01.png'
+            width={1200}
+            height={720}
+            alt='Título do post'
+            priority
+          />
         </Link>
-        <div></div>
+        <div className='flex flex-col gap-4'>
+          <time className='text-slate-600 text-sm' dateTime='2025-06-14'>
+            14/06/2026 10:00
+          </time>
+
+          <h1>
+            <Link href='#'>Clique para testar</Link>
+          </h1>
+
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat,
+            minus natus voluptates, ratione sunt alias enim corrupti iure quo
+            cumque blanditiis suscipit consequatur amet aperiam. Rerum nihil
+            molestiae sequi fuga.
+          </p>
+        </div>
       </section>
 
       <Suspense fallback={<SpinLoader />}>
